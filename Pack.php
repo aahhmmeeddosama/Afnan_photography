88a3f9d1b3f4980022b4358846577e117f232cc8
aahhmmeeddosama <67119304+aahhmmeeddosama@users.noreply.github.com>
2021-05-10 02:16:02 +0200

    Update Pack.php

--- Pack.php
+++ Pack.php
@@ -1,30 +1,13 @@
 
 <html>
 <head>
-<<<<<<< HEAD
-	<link rel="stylesheet" type="text/css" href="pack.css">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald|Noto+Sans">
-=======
     <link rel="stylesheet" type="text/css" href="pack.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald|Noto+Sans">
->>>>>>> 0367b0d4a216f71e35ed24a8f996d8143ca8d53a
     <link rel="shortcut icon"  href="Images/LB_Editedb.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 </head>
 <body>
-<<<<<<< HEAD
-	<body>
-	<div class="pack">	
-	<div class="col">
-		<div class="title"><h2>Weeding</h2></div>
-	<img src="packphotos/3.jpeg">
-	<div class="content">
-		<ul>
-			<li>
-    			<div class="dropdown">
-=======
     <body>
     <div class="pack">  
     <div class="col">
@@ -34,7 +17,6 @@
         <ul>
             <li>
                 <div class="dropdown">
->>>>>>> 0367b0d4a216f71e35ed24a8f996d8143ca8d53a
                     <span>Cairo</span>
                     <div class="dropdown-content">
                         <p>Start from 1000EG to 3000EG</p>
@@ -66,13 +48,8 @@
                 </div>
             </li>
 
-<<<<<<< HEAD
-		</ul>
-		<div class="triangle-up"></div>
-=======
         </ul>
         <div class="triangle-up"></div>
->>>>>>> 0367b0d4a216f71e35ed24a8f996d8143ca8d53a
         <div class ="btn">
             <a class ="glow" href="BOOK.php">Book now</a>
         </div>
@@ -82,16 +59,6 @@
     </div>
 
 <div class="col">
-<<<<<<< HEAD
-	<div class="title"><h2>Engagment</h2></div>
-	<img src="packphotos/1.jpeg">
-	<div class="content">
-		
-		<ul>
-			
-			<li>
-    			<div class="dropdown">
-=======
     <div class="title"><h2>Engagment</h2></div>
     <img src="packphotos/1.jpeg">
     <div class="content">
@@ -100,7 +67,6 @@
             
             <li>
                 <div class="dropdown">
->>>>>>> 0367b0d4a216f71e35ed24a8f996d8143ca8d53a
                     <span>Cairo</span>
                     <div class="dropdown-content">
                         <p>Start from 1000EG to 3000EG</p>
@@ -132,23 +98,6 @@
                 </div>
             </li>
 
-<<<<<<< HEAD
-		</ul>
-			<div class="triangle-up"></div>
-        <div class ="btn">
-            <a class ="glow" href="BOOK.php">Book now</a>
-        </div>
-	</div>
-</div>
-	<div class="col">
-	<div class="title"><h2>Casual</h2></div>
-	<img src="packphotos/2.jpeg">
-	<div class="content">
-		<ul>
-			
-			<li>
-				<div class="dropdown">
-=======
         </ul>
             <div class="triangle-up"></div>
         <div class ="btn">
@@ -164,7 +113,6 @@
             
             <li>
                 <div class="dropdown">
->>>>>>> 0367b0d4a216f71e35ed24a8f996d8143ca8d53a
                     <span>Alex</span>
                     <div class="dropdown-content">
                         <p>Start from 1000EG to 3000EG</p>
@@ -196,23 +144,6 @@
                 </div>
             </li>
 
-<<<<<<< HEAD
-		</ul>
-		<div class="triangle-up"></div>
-        <div class ="btn">
-            <a class ="glow" href="BOOK.php">Book now</a>
-        </div>
-	</div>
-</div>
-	<div class="col">
-	<div class="title"><h2 id="other">Other</h2></div>
-	<img src="packphotos/4.jpeg">
-	<div class="content">
-		<ul>
-			
-			<li>
-    			<div class="dropdown">
-=======
         </ul>
         <div class="triangle-up"></div>
         <div class ="btn">
@@ -228,7 +159,6 @@
             
             <li>
                 <div class="dropdown">
->>>>>>> 0367b0d4a216f71e35ed24a8f996d8143ca8d53a
                     <span>Cairo</span>
                     <div class="dropdown-content">
                         <p>Start from 1000EG to 3000EG</p>
@@ -260,17 +190,6 @@
                 </div>
             </li>
 
-<<<<<<< HEAD
-		</ul>
-		<div class="triangle-up"></div> 
-        <div class ="btn">
-            <a class ="glow" href="BOOK.php">Book now</a>
-        </div>
-		
-	</div>
-	</div>
-	</div>
-=======
         </ul>
         <div class="triangle-up"></div> 
         <div class ="btn">
@@ -280,7 +199,6 @@
     </div>
     </div>
     </div>
->>>>>>> 0367b0d4a216f71e35ed24a8f996d8143ca8d53a
 </body>
 </html>
 <?php include "Main_Nav.php";?>
