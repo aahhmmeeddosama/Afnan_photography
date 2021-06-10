f4634b9c2e4b230257ef85078082f0e3fc8396f5
aahhmmeeddosama <67119304+aahhmmeeddosama@users.noreply.github.com>
2021-06-10 03:24:42 +0200

    final BOOK isa

--- database_Connect.php
+++ database_Connect.php
@@ -12,17 +12,7 @@ protected function connect(){
     $pdo=new PDO($conn,$this->username,$this->pass);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
     return $pdo;
-    /*try{
-        $this->connect = new mysqli($host, $user, $pass, $db_name);
-        if(mysqli_connect_error()){
-           die('connection error('.mysqli_connect_errno().')' . mysqli_connect_error());
-        }
-    }
 
-    catch(Exception $e){
-        echo $e->getMessage();
-        }
-    }*/
 }
 }
 ?>
